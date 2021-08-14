cc515ee5b5390df98aa38ea4c05b3010b9093cfd
Vishwajeet Raj <vishwajeetraj11@gmail.com>
2021-08-14 17:25:51 +0530

    search user implementation

--- src/api/controllers/userController.js
+++ src/api/controllers/userController.js
@@ -1,4 +1,3 @@
-import { Project } from '../../models/Project.js';
 import { User } from '../../models/User.js';
 import { AppError } from '../../utils/AppError.js';
 import { catchAsync } from '../../utils/catchAsync.js';
@@ -7,9 +6,6 @@ export const getProfile = catchAsync(async (req, res, next) => {
   const user = await User.findById(req.user.userId).select(
     '-transaction -client_id -tenant -request_language'
   );
-  // .populate({ path: 'projects' });
-
-  // await Project.populate(user, )
 
   if (!user) {
     return next(new AppError('No user found.', 404));
@@ -39,22 +35,26 @@ export const updateProfile = catchAsync(async (req, res, next) => {
 });
 
 export const getUsers = catchAsync(async (req, res, next) => {
-  const keyword = req.query.keyword
+  console.log(req.query.keyword);
+  const filter = req.query.keyword
     ? {
-        name: {
-          $regex: req.query.keyword,
-          $options: 'i',
-        },
+        $or: [
+          {
+            email: {
+              $regex: req.query.keyword,
+              $options: 'i',
+            },
+          },
+          {
+            username: {
+              $regex: req.query.keyword,
+              $options: 'i',
+            },
+          },
+        ],
       }
     : {};
-  const count = await User.countDocuments({ ...keyword });
-  const users = await Product.find({ ...keyword });
-  // .limit(pageSize)
-  // .skip(pageSize * (page - 1));
-  res.json({ count, users });
-
-  res.status(200).json({
-    status: 'success',
-    users: 'Array of users found.',
-  });
+  const count = await User.countDocuments(filter);
+  const users = await User.find(filter);
+  res.status(200).json({ status: 'success', count, users });
 });
